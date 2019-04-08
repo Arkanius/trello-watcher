b66c8eff06a4e1136ad1fac43a19a88933eb09e8
Victor <victor@victor-note.victor>
2019-04-08 00:01:06 -0300

    getting cards

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use GuzzleHttp\Client;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,5 +13,14 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+//    return view('welcome');
+
+    $key = env('TRELLO_KEY');
+    $token = env('TRELLO_TOKEN');
+    $board = '5a8c25ea560c1c1ca1347546';
+
+    $client = new GuzzleHttp\Client(['base_uri' => 'https://api.trello.com']);
+// Send a request to https://foo.com/api/test
+    $response = $client->request('GET', "1/boards/$board/lists?key=$key&token=$token");
+    dd($response->getBody()->getContents());
 });
