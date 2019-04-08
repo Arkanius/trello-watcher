527010f8c1ca51a91118775600b1857dddb0e347
Victor Gazotti <vtr.gomes@hotmail.com>
2019-04-08 00:19:54 -0300

    testing api

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,18 @@ use GuzzleHttp\Client;
 */
 
 Route::get('/', function () {
-//    return view('welcome');
-
     $key = env('TRELLO_KEY');
     $token = env('TRELLO_TOKEN');
-    $board = '5a8c25ea560c1c1ca1347546';
+    $board = env('TRELLO_BOARD');
+    $action = 'cards';
+    $action = 'actions'; //$content->date
 
     $client = new GuzzleHttp\Client(['base_uri' => 'https://api.trello.com']);
-// Send a request to https://foo.com/api/test
-    $response = $client->request('GET', "1/boards/$board/lists?key=$key&token=$token");
-    dd($response->getBody()->getContents());
+    $response = $client->request('GET', "1/boards/$board/$action?key=$key&token=$token");
+
+    foreach (json_decode($response->getBody()->getContents()) as $content) {
+        dd($content);
+        dd($content->id, $content->name, $content->shortUrl, $content->labels,
+            $content->idList);
+    }
 });
