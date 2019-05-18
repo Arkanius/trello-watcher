958d25292cd537d147596e2d0ad9677af152bd93
Victor Gazotti <vtr.gomes@hotmail.com>
2019-05-18 12:58:22 -0300

    listing boards

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('boards')->group(function () {
+    Route::get('/', 'BoardController@index')->name('boards.all');
+});
+
